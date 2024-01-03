3df9e811aa8b0469ea2d5e903f2dd16484b9e1d1
brin <budiaramdhanrindi@gmail.com>
2024-01-03 13:40:07 +0700

    init constant.go

---
+++ constant.go
@@ -0,0 +1,17 @@
+package constant
+
+const (
+	CtxSessionIDKey = "SessionID"
+	CtxClientIDKey  = "ClientID"
+	CtxUserIDKey    = "UserID"
+
+	HeaderSessionIDKey = "X-Session-ID"
+	HeaderClientIDKey  = "X-Client-ID"
+	HeaderUserIDKey    = "X-User-ID"
+
+	LogCategoryApp      = "app"
+	LogCategoryUsecase  = "usecase"
+	LogCategoryExternal = "external"
+
+	ServiceName = "pajak"
+)
