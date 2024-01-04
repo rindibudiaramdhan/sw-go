a6600968b91fcee7b63c6e654b1f049b77aecda2
brin <budiaramdhanrindi@gmail.com>
2024-01-04 14:00:40 +0700

    init main.go and go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rindibudiaramdhan/sw-go
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rindibudiaramdhan/sw-go/constant"
+	"github.com/rindibudiaramdhan/sw-go/helper/numbers"
+)
+
+func main() {
+	fmt.Println(constant.CtxClientIDKey)
+
+	fmt.Println(numbers.IsPrime(19))
+}
