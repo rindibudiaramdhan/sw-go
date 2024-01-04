435f3c1d06486fa5dd00e1c92fc0f78211a545e6
brin <budiaramdhanrindi@gmail.com>
2024-01-04 13:59:16 +0700

    feat(time): get expired date time

--- helper/time.go
+++ helper/time.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"strconv"
 	"time"
 )
 
@@ -21,4 +22,27 @@ func ParseInLocation(layout string, value string, locationName string) (*time.Ti
 	}
 
 	return &expiredAt, nil
+}
+
+func GetExpiredDateTime(t string) string {
+	now := time.Now().UTC()
+	yyyy, m, dd := now.Date()
+
+	monthInt := int(m)
+	monthStr := strconv.Itoa(monthInt)
+	if monthInt < 10 {
+		monthStr = "0" + strconv.Itoa(monthInt)
+	}
+
+	day := strconv.Itoa(dd)
+	if dd < 10 {
+		day = "0" + strconv.Itoa(dd)
+	}
+
+	if now.Hour() >= 15 {
+		day = strconv.Itoa(dd + 1)
+	}
+
+	expiredDatetime := strconv.Itoa(yyyy) + "-" + monthStr + "-" + day + " " + t
+	return expiredDatetime
 }
